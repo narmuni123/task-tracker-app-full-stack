0e84364ebeeb761b1cb91cb4566029edc2910ceb
narmuni123 <narmuni123@gmail.com>
2025-10-18 22:44:43 +0530

    Added entity for User and Task

---
+++ taskManager/src/main/java/com/munikiran/taskManager/model/Status.java
@@ -0,0 +1,7 @@
+package com.munikiran.taskManager.model;
+
+public enum Status {
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
---
+++ taskManager/src/main/java/com/munikiran/taskManager/model/User.java
@@ -0,0 +1,32 @@
+package com.munikiran.taskManager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> tasks = new ArrayList<>();
+}
